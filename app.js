f42f52a825e0853b0599223fa00b7dda5707fce8
yeaheun3 <155213424+yeaheun3@users.noreply.github.com>
2024-01-29 20:43:15 +0900

    classList로 특정한 className만 변경해주기

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ console.dir(h1);
 function handleTitleClick() {
   //raw value를 사용하는 대신 변수로 만들어준다.(clicked를 계속 쓰지 않아도 됨, 변수는 오타도 잡아낼 수 있음)
   const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
+  // 원래 h1에 지정해준 className은 냅두고 특정한 className만 변경하도록 하기
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = clickedClass;
+    h1.classList.add(clickedClass);
   }
 }
 
