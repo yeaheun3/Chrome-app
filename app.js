85520d3f75d6eacfd6fa237a8f75875bc6a6a07a
yeaheun3 <155213424+yeaheun3@users.noreply.github.com>
2024-01-29 20:31:47 +0900

    CSS in Javascript 2

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ console.dir(h1);
 
 //animation을 위한 tool은 javaScript, style을 위한 tool은 CSS
 function handleTitleClick() {
-  if (h1.className === "active") {
+  //raw value를 사용하는 대신 변수로 만들어준다.(clicked를 계속 쓰지 않아도 됨, 변수는 오타도 잡아낼 수 있음)
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
     h1.className = "";
   } else {
-    h1.className = "active";
+    h1.className = clickedClass;
   }
 }
 
