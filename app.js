3db8244c46e3a3a469bc826606525820c641fbc5
yeaheun3 <155213424+yeaheun3@users.noreply.github.com>
2024-01-29 20:52:43 +0900

    classList.toggle Method 이용해서 Class 추가하기

--- app.js
+++ app.js
@@ -4,14 +4,8 @@ console.dir(h1);
 
 //animation을 위한 tool은 javaScript, style을 위한 tool은 CSS
 function handleTitleClick() {
-  //raw value를 사용하는 대신 변수로 만들어준다.(clicked를 계속 쓰지 않아도 됨, 변수는 오타도 잡아낼 수 있음)
-  const clickedClass = "clicked";
-  // 원래 h1에 지정해준 className은 냅두고 특정한 className만 변경하도록 하기
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
+  // classList.toggle -> 요소의 class 목록에 class가 이미 존재하면 제거하고, 없으면 추가하는 method
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
